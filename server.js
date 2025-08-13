e740e87cb8ff6a5f4e8eee682d358e9da192ab2f
laxmimagar <laxmithapamagar908@gmail.com>
2025-08-13 12:23:39 +0545

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Static files serve karne ke liye
+app.use(express.static(path.join(__dirname)));
+
+// Root route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
